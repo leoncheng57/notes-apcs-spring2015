8df2450bb71c3619dad7fe711c3936989769c20b
leoncheng57 <57leonardo@gmail.com>
2015-02-28 18:31:30 -0500

    short studying on constructors and inheritance

---
+++ 2-study/Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+    public static void main(String[]args){
+	Foo f = new Foo(2);
+	Sub s = new Sub();
+	f.printMe();
+	s.printMe();
+    }
+}
---
+++ 2-study/Foo.java
@@ -0,0 +1,10 @@
+public class Foo{
+    
+    public Foo(int a){
+	System.out.println("Inside Foo constructor");
+    }
+
+    public void printMe(){
+	System.out.println("I am in Foo!");
+    }
+}
